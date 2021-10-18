258e07f5eb8845d2f0d23e5e52df495f1d129c22
Bleriot Noguia <nstevebleriot@yahoo.fr>
2021-10-18 12:45:40 +0100

    Logout mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import query from '../queries/CurrentUser'
+import mutation from '../mutations/Logout'
 import {Link} from 'react-router'
 
 class Header extends Component {
+  onLogoutClick(){
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    })
+  }
+
   renderButtons(){
     const {loading, user} = this.props.data;
     if(loading){
       return <div />;
     }
     if(user){
-      return <div>Logout</div>;
+      return (
+        <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
+      );
     }else{
       return (
         <div>
@@ -40,4 +49,6 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+  graphql(query)(Header)
+);
